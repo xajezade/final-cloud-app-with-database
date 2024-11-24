a9bf37900af2bf666ce154dd65235db3c6b3faa5
xajezade <ali.xajezade@gmail.com>
2024-11-24 16:59:18 -0500

    completed project

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-# <HINT> Import any new Models here
-from .models import Course, Enrollment
+from .models import Course, Enrollment, Question, Choice, Submission
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
 from django.urls import reverse
@@ -11,7 +10,34 @@ import logging
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 # Create your views here.
+def submit(request, course_id):
+    course = get_object_or_404(Course, pk=course_id)
+    user = request.user
+    enrol = Enrollment.objects.get(user=user, course=course)
+    submission = Submission.objects.create(enrollment=enrol)
+    choices = extract_answers(request)
+    submission.choices.set(choices)
+    submission_id = submission.id
+    submission.save()
+    return HttpResponseRedirect(reverse(viewname='onlinecourse:exam_result', args=(course.id,submission_id)))
 
+def show_exam_result(request, course_id, submission_id):
+    context = {}
+    course = get_object_or_404(Course, pk=course_id)
+    submission = Submission.objects.get(id=submission_id)
+    choices = submission.choices.all()
+    total_score = 0
+    questions = course.question_set.all()
+
+    for question in questions:
+        correct_choices = question.choice_set.filter(is_correct=True)
+        selected_choices = choices.filter(question=question)
+        if set(correct_choices) == set(selected_choices):
+            total_score +=question.grade
+    context['course'] = course
+    context['grade'] = total_score
+    context['choices'] = choices
+    return render(request, 'onlinecourse/exam_result_bootstrap.html', context)
 
 def registration_request(request):
     context = {}
